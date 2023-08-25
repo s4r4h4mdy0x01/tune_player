c494432768c1ae11a131ae5865e0697e0c4d5163
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2023-08-26 01:56:44 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tune_player/pages/tunePage.dart';
+
+void main() {
+  runApp(const TuneAPP());
+}
+
+class TuneAPP  extends StatelessWidget {
+  const TuneAPP({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      
+      home: const TunePage()
+    );
+  }
+}
+
---
+++ lib/pages/tunePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TunePage extends StatelessWidget {
+  const TunePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar:  AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.grey[800],
+      title: const Text('Tune App '),
+
+    ),
+    body: Column(
+      children: [
+        
+      ],
+    ),
+
+
+    );
+  }
+}
